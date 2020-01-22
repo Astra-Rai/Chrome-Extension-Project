1bbfd4d53a2e0f95cff919802f363e5ee837d925
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2020-01-22 13:09:16 -0500

    Initial Commit

---
+++ Chrome_Extension_Project/content.js
@@ -0,0 +1,16 @@
+var elements = document.getElementsByTagName('*');
+for (var i = 0; i < elements.length; i++){
+    var element = elements[i];
+        for(var j = 0; j < element.childNodes.length; j++){
+            var node = element.childNodes[j];      
+        
+            if(node.nodeType === 3 || node.nodeType === 2) {
+                var text = node.nodeValue;
+                var replacedText = text.replace(/excon/gi, 'person with justice system involvement');
+            
+            if(replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
+        }   
+    }
+}
